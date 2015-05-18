5130707dbea24fcc1f27fb98f14b0209855383ad
xipetotec <xipetotec@AZTLAN>
2015-05-17 22:01:50 -0500

    testing comitt

--- src/main/java/org/nekorp/workflow/desktop/control/imp/AuthorityControllerImp.java
+++ src/main/java/org/nekorp/workflow/desktop/control/imp/AuthorityControllerImp.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License
  */
-
 package org.nekorp.workflow.desktop.control.imp;
 
 import org.aspectj.lang.JoinPoint;
